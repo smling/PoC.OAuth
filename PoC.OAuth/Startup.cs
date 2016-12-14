ca0f0160ed380bb8500c26b11ef6bf83eb53d792
chunholing <chunholing@PC0238V-VS2015>
2016-12-14 16:55:00 +0800

    Add project files.

---
+++ PoC.OAuth/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PoC.OAuth.Startup))]
+namespace PoC.OAuth
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
